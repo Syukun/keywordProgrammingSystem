3b7ed0e3746ca656fa7d62e05ed81415328fd94b
Aochi <shuaochisoft@yahoo.co.jp>
2019-07-24 10:50:09 +0900

    Extract part is kind of finished

--- keywordProgrammingSystem/src/dataBase/DataFromSourceFile.java
+++ keywordProgrammingSystem/src/dataBase/DataFromSourceFile.java
@@ -1,6 +1,6 @@
 package dataBase;
 
-
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -11,6 +11,7 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IField;
 import org.eclipse.jdt.core.IImportDeclaration;
+import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IPackageFragment;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.ITypeHierarchy;
@@ -19,7 +20,6 @@ import org.eclipse.jdt.core.Signature;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.Type;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
 import org.eclipse.jdt.core.search.IJavaSearchScope;
 import org.eclipse.jdt.core.search.SearchEngine;
@@ -34,33 +34,35 @@ public class DataFromSourceFile {
 
 	ContentAssistInvocationContext context;
 	IProgressMonitor monitor;
-	
+
 	private Vector<IType> allITypesFromSourceFile;
 	private ICompilationUnit icu;
 	// (name : IType) -> (SimpleName : String) -> TypeF
 //	Map<IType,Map<String,TypeF>> typeDictionary;
 	// now just consider the type in current class
 	private Map<String, TypeF> typeDictionary;
-	
+
 	private Map<String, TypeF> localVariables;
-	
+
 	private Set<Field> fields;
 
-	public DataFromSourceFile(ContentAssistInvocationContext context, IProgressMonitor monitor) throws JavaModelException {
+	private Set<Method> methods;
+
+	public DataFromSourceFile(ContentAssistInvocationContext context, IProgressMonitor monitor)
+			throws JavaModelException {
 		this.context = context;
 		this.monitor = monitor;
 		this.setThisICompilationUnit();
 		this.setAllITypesFromSourceFile();
 		this.setTypeDictionary();
 //		this.typeDictionary = new HashMap<IType, Map<String, TypeF>>();
-		this.localVariables = new HashMap<String, TypeF>();
 		this.setLocalVariables();
-		this.fields = new HashSet<Field> ();
 		this.setFields();
+		this.setMethods();
 	}
-	
+
 	public void setThisICompilationUnit() {
-		this.icu = ((JavaContentAssistInvocationContext) context).getCompilationUnit();		
+		this.icu = ((JavaContentAssistInvocationContext) context).getCompilationUnit();
 	}
 
 	/**
@@ -82,11 +84,11 @@ public class DataFromSourceFile {
 		this.allITypesFromSourceFile.addAll(getAllITypes(icu));
 
 	}
-	
-	private Map<String,TypeF> getPrimitiveTypes(){
-		Map<String,TypeF> res = new HashMap<String,TypeF>();
-		String[] priTypes = {"byte","int","short","long","char","double","boolean","float"};
-		for(String priType : priTypes) {
+
+	private Map<String, TypeF> getPrimitiveTypes() {
+		Map<String, TypeF> res = new HashMap<String, TypeF>();
+		String[] priTypes = { "byte", "int", "short", "long", "char", "double", "boolean", "float" };
+		for (String priType : priTypes) {
 			res.put(priType, new TypeF(priType));
 		}
 		return res;
@@ -95,11 +97,11 @@ public class DataFromSourceFile {
 	public void setTypeDictionary() throws JavaModelException {
 		typeDictionary = new HashMap<String, TypeF>();
 		typeDictionary.putAll(getPrimitiveTypes());
-		for(IType iType : allITypesFromSourceFile) {
+		for (IType iType : allITypesFromSourceFile) {
 			String iTypeName = iType.getElementName();
-			typeDictionary.put(iTypeName, new TypeF(iType,monitor));
+			typeDictionary.put(iTypeName, new TypeF(iType, monitor));
 		}
-		
+
 	}
 
 	public Vector<IType> getAllITypes(ICompilationUnit icu) throws JavaModelException {
@@ -144,56 +146,57 @@ public class DataFromSourceFile {
 
 			se.searchAllTypeNames(packageName, packageMatchRule, typeName, typeMatchRule, searchFor, scope,
 					nameMatchRequestor, waitingPolicy, monitor);
-			
+
 			IType importDeclarationType = nameMatchRequestor.getIType();
 			res.add(importDeclarationType);
 			res.addAll(getAllSubITypes(importDeclarationType));
 
 		}
-		
+
 		/**
 		 * TODO Extract all ITypes from java.lang.*;
 		 */
 		char[] packageNameLang = "java.lang".toCharArray();
 		MyTypeNameMatchRequestor nameMatchRequestorLang = new MyTypeNameMatchRequestor();
-		se.searchAllTypeNames(packageNameLang, packageMatchRule, null, 0, searchFor, scope,
-				nameMatchRequestorLang, waitingPolicy, monitor);
+		se.searchAllTypeNames(packageNameLang, packageMatchRule, null, 0, searchFor, scope, nameMatchRequestorLang,
+				waitingPolicy, monitor);
 		Vector<IType> iTypesLang = nameMatchRequestorLang.getITypes();
 		res.addAll(iTypesLang);
-		
+
 		return res;
 	}
 
-
 	private char[] getPackageName(String iimdName, int lastDotPos) {
 		return iimdName.substring(0, lastDotPos).toCharArray();
-		 
+
 	}
 
 	private char[] getTypeName(String iimdName, int lastDotPos) {
 		return iimdName.substring(lastDotPos + 1).toCharArray();
 	}
-	
+
 	private Vector<IType> getAllSubITypes(IType importDeclarationType) throws JavaModelException {
 		Vector<IType> res = new Vector<IType>();
 		ITypeHierarchy ith = importDeclarationType.newTypeHierarchy(monitor);
 		IType[] subITypes = ith.getAllSubtypes(importDeclarationType);
-		for(IType subIType : subITypes) {
+		for (IType subIType : subITypes) {
 			res.add(subIType);
 		}
 		return res;
 	}
-	
+
 	public Vector<IType> getAllITypesFromSourceFile() {
 		return allITypesFromSourceFile;
 	}
-	
+
 	/**
 	 * extract all local variables by ASTParser
-	 * @throws JavaModelException 
+	 * 
+	 * @throws JavaModelException
 	 */
 	public void setLocalVariables() throws JavaModelException {
-		//Step-1 : create a parser
+		this.localVariables = new HashMap<String, TypeF>();
+		// Step-1 : create a parser
 		ASTParser parser = ASTParser.newParser(AST.JLS11);
 		parser.setSource(icu);
 		CompilationUnit cu = (CompilationUnit) parser.createAST(monitor);
@@ -202,44 +205,84 @@ public class DataFromSourceFile {
 		// Use ASTVisitor to get This Type and Local Variables
 		MyVisitor mv = new MyVisitor(cursorPos);
 		cu.accept(mv);
-		
-		//Step-2 : Use ASTVisitor
-		Map<String,String> localVariables_AST = mv.getLocalVariables();
-		
-		for(String localVarName : localVariables_AST.keySet()) {
+
+		// Step-2 : Use ASTVisitor
+		Map<String, String> localVariables_AST = mv.getLocalVariables();
+
+		for (String localVarName : localVariables_AST.keySet()) {
 			String localTypeName = localVariables_AST.get(localVarName);
 			TypeF localTypeF = typeDictionary.get(localTypeName);
 			this.localVariables.put(localVarName, localTypeF);
 		}
-		
+
 	}
-	
+
+	public Map<String, TypeF> getLocalVariables(){
+		return this.localVariables;
+	}
 	/**
-	 * extract all fields 
+	 * extract all fields
+	 * 
 	 * @throws JavaModelException
 	 */
-	public void setFields() throws JavaModelException{
-		for(IType iType : this.allITypesFromSourceFile) {
+	public void setFields() throws JavaModelException {
+		this.fields = new HashSet<Field>();
+		for (IType iType : this.allITypesFromSourceFile) {
 			IField[] iFields = iType.getFields();
-			for(IField iField : iFields) {
-				TypeF classType = new TypeF(iType,monitor);
+			TypeF classType = new TypeF(iType, monitor);
+			for (IField iField : iFields) {
 				String iFieldName = iField.getElementName();
 				String iFieldTypeSig = iField.getTypeSignature();
-				String iFieldType = Signature.getSignatureSimpleName(iFieldTypeSig);
-				TypeF iFieldTypeF = typeDictionary.get(iFieldType);
-				
-				if(iFieldTypeF != null) {
+				TypeF iFieldTypeF = this.getTypeFFromSign(iFieldTypeSig);
+
+				if (iFieldTypeF != null) {
 					this.fields.add(new Field(classType, iFieldTypeF, iFieldName));
 				}
 			}
 		}
 	}
- 
 	
-	public void setMethods() throws JavaModelException{
-		
+	public Set<Field> getFields(){
+		return this.fields;
 	}
 
+	public void setMethods() throws JavaModelException {
+		this.methods = new HashSet<Method>();
+		for (IType iType : this.allITypesFromSourceFile) {
+			IMethod[] iMethods = iType.getMethods();
+			TypeF receiveTypeF = new TypeF(iType, monitor);
+
+			for (IMethod iMethod : iMethods) {
+				String methodName = iMethod.getElementName();
+
+				String returnTypeSig = iMethod.getReturnType();
+				TypeF returnTypeF = this.getTypeFFromSign(returnTypeSig);
+
+				String[] parameterTypeSig = iMethod.getParameterTypes();
+//				Stream<String> typeSignStream = Stream.of(parameterTypeSig);
+//				typeSignStream.forEach(x -> getTypeFFromSign(x));
+//				TypeF[] parameterTypesF = typeSignStream.toArray(TypeF[]::new);
+
+				TypeF[] parameterTypesF = Arrays.stream(parameterTypeSig).map(x -> getTypeFFromSign(x))
+						.toArray(TypeF[]::new);
+
+				Method method = new Method(methodName, receiveTypeF, returnTypeF, parameterTypesF);
+				this.methods.add(method);
+			}
+		}
+	}
+
+	private TypeF getTypeFFromSign(String signature) {
+		String typeName = Signature.getSignatureSimpleName(signature);
+		return this.typeDictionary.get(typeName);
+	}
+	
+	public Set<Method> getMethods(){
+		return this.methods;
+	}
+	
+	
+	
 //	private IType findThisIType(String nameOfThis) throws JavaModelException {
 //		IType[] currentTypes = icu.getTypes();
 //		for(IType type : currentTypes) {
--- keywordProgrammingSystem/src/dataBase/Method.java
+++ keywordProgrammingSystem/src/dataBase/Method.java
@@ -1,32 +1,17 @@
 package dataBase;
 
-import org.eclipse.jdt.core.IMethod;
-import org.eclipse.jdt.core.JavaModelException;
-
 public class Method {
-	private IMethod method;
-	
-	// TODO finish under stuff.
-	
-	public Method(IMethod method) {
-		this.method = method;
-	}
-	
-	public String getMethodName() {
-		return method.getElementName();
-	}
-	
-	//TODO find the proper hierarchy of it
-	public String getReturnName() throws JavaModelException {
-		return method.getReturnType();
-	}
-	
-	public String[] getParameterNames() {
-		return method.getParameterTypes();
-	}
-	//TODO find proper hierarchy
-	public String getType() {
-		return method.getTypeRoot().getElementName();
+	String methodName;
+	TypeF receiveType;
+	TypeF returnType;
+	TypeF[] parameterTypes;
+//	TypeF[] exceptionTypes;
+	public Method(String methodName, TypeF receiveType, TypeF returnType, TypeF[] parameterTypes) {
+		super();
+		this.methodName = methodName;
+		this.receiveType = receiveType;
+		this.returnType = returnType;
+		this.parameterTypes = parameterTypes;
 	}
 	
 	
